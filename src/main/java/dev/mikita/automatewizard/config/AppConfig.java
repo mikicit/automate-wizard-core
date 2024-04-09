24b2ac81920a87b864f685ffef14d97d854388f4
Mikita Citarovič <titorovichnikita@gmail.com>
2024-04-09 19:53:43 +0200

    deletion of unnecessary serialiser

--- src/main/java/dev/mikita/automatewizard/config/AppConfig.java
+++ src/main/java/dev/mikita/automatewizard/config/AppConfig.java
@@ -3,13 +3,10 @@ package dev.mikita.automatewizard.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import dev.mikita.automatewizard.util.HttpMethodSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -25,9 +22,6 @@ public class AppConfig {
     @Primary
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addSerializer(HttpMethod.class, new HttpMethodSerializer());
-        objectMapper.registerModule(module);
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
