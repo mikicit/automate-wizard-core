595d1068259bacdb3bf3324974211780ae862f26
Mikita Citarovič <titorovichnikita@gmail.com>
2024-05-07 19:40:19 +0200

    unnecessary imports removed

--- src/main/java/dev/mikita/automatewizard/controller/WebhookController.java
+++ src/main/java/dev/mikita/automatewizard/controller/WebhookController.java
@@ -6,9 +6,6 @@ import dev.mikita.automatewizard.service.ScenarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 @RestController
